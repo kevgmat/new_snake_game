138da16031d789064f9277cef7c0603b5312c8ca
kevin george mathew <kevgmat@gmail.com>
2023-02-27 20:14:42 +0530

    created background

--- main.py
+++ main.py
@@ -1,16 +1,7 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+import pygame
+pygame.init()
+display = pygame.display.set_mode((620, 720))
+background = pygame.Surface((620, 720))
+background.fill((0, 0, 0))
+pygame.display.update()
+pygame.time.delay(5000)
