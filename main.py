17dbac4ed66a163eb08ec7b95ee24a019107ea6e
kevin george mathew <kevgmat@gmail.com>
2023-02-28 02:57:50 +0530

    drawing_random_food

--- main.py
+++ main.py
@@ -5,6 +5,20 @@ pygame.init()
 display = pygame.display.set_mode((620, 720))
 background = pygame.Surface((620, 720))
 background.fill((0, 0, 0))
+food_pos = []
+
+i = 1
+percentage = 50
+while i<= int((percentage/100)*961):
+    x = random.randint(1,31)
+    y = random.randint(1,31)
+    if (x,y) in food_pos:
+        pass
+    else:
+        food_pos.append((x,y))
+        pygame.draw.rect(display, pygame.Color((0, 0, 255)), [((x - 1) * 20), ((y - 1) * 20), 20, 20])
+        i+=1
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -16,10 +30,6 @@ while True:
 
         pygame.draw.line(display, pygame.Color((0, 255, 0)), (0, ((i+31) * 20)), (620, ((i+31) * 20)), width=1)
         pygame.draw.line(display, pygame.Color((0, 255, 0)), ((i*20),620),((i*20),720), width = 1)
-
-    x = random.randint(1,31)
-    y = random.randint(1,31)
-    pygame.draw.rect(display, pygame.Color((0,0,255)), [((x-1)*20), ((y-1)*20), 20, 20])
 
     pygame.display.update()
 pygame.quit()
